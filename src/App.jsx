9f0e957b6455f0983fecaa25321cebfb69fe9be2
Mrkan <mrkan.alelgn1@gmail.com>
2026-05-13 10:31:02 +0300

    feat: add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello, world</h1>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Hello, world</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
